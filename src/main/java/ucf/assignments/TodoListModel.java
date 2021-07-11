dcf440d2fea2e321be3bd5f28e1a8768844aa164
dang <dangh2809@gmail.com>
2021-07-11 16:31:33 -0400

    solution for assignment 4

--- src/main/java/ucf/assignments/TodoListModel.java
+++ src/main/java/ucf/assignments/TodoListModel.java
@@ -1,4 +1,47 @@
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 Hieu Dang
+ */
 package ucf.assignments;
 
+import java.util.ArrayList;
+
 public class TodoListModel {
+    private ArrayList<TodoItem> list;
+
+    public void addItem(TodoItem item){
+
+    }
+    public void removeItem(TodoItem item){
+
+    }
+    public TodoItem searchItem(String title) {
+
+    }
+    public void editItemTitle(String title){
+
+    }
+    public void editItemDueDate(String dueDate){
+
+    }
+    public void editItemDescription(String description){
+
+    }
+    public void editIsComplete(Boolean isComplete){
+
+    }
+    public void save(ArrayList<TodoItem> list){
+
+    }
+    public void clear(ArrayList<TodoItem>){
+
+    }
+    public void displayIncomplete(ArrayList<TodoItem>){
+
+    }
+    public void displayCompleted(ArrayList<TodoItem>){
+
+    }
+    +load()
 }
